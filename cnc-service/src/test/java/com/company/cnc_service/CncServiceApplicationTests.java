02d5251809fd153eb6bd34b698a14d9fc7027948
RomilRabadiya <romilrabadiya@gmail.com>
2025-12-12 00:39:38 +0530

    Project StartUp

---
+++ cnc-service/src/test/java/com/company/cnc_service/CncServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.cnc_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CncServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
